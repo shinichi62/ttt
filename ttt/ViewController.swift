734ebcbde88af0ef9a079c1ec3721affd7757897
shinichi yamaguchi <shinichi62@gmail.com>
2017-07-13 10:02:12 +0900

    add save button action

--- ttt/ViewController.swift
+++ ttt/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tokenField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapSaveButton(_ sender: Any) {
+        print("tap save button!" + tokenField.text!)
+    }
 
 }
 
