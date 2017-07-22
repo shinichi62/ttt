f51c0fb0864d4393b7faec2bac7e33be2e67af05
shinichi yamaguchi <shinichi62@gmail.com>
2017-07-23 07:54:30 +0900

    Revert "Add Alamofire library"
    This reverts commit 50eb8e8e6efd8d8a48dcb021d8ffff92a6a0e2fc.

--- ttt WatchKit Extension/InterfaceController.swift
+++ ttt WatchKit Extension/InterfaceController.swift
@@ -8,7 +8,6 @@
 
 import WatchKit
 import Foundation
-import Alamofire
 
 
 class InterfaceController: WKInterfaceController {
@@ -21,27 +20,6 @@ class InterfaceController: WKInterfaceController {
         // Configure interface objects here.
         myTimer.start()
         myTimer.setDate(Date(timeIntervalSinceNow: TimeInterval(-210)))
-
-        // Get running time entry
-        Alamofire.request("https://www.toggl.com/api/v8/time_entries/current")
-            .authenticate(user: tokenxxx, password: api_token )
-            .responseJSON { response in
-                print("Request: \(String(describing: response.request))")   // original url request
-                print("Response: \(String(describing: response.response))") // http url response
-                print("Result: \(response.result)")                         // response serialization result
-
-                if let json = response.result.value {
-                    print("JSON: \(json)") // serialized json response
-                }
-
-                if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
-                    print("Data: \(utf8Text)") // original server data as UTF8 string
-            }
-        }
-
-        // Get running time entry
-        //let toggl = Toggl('tokenxxx')
-        //let entry = toggl.getRunningTImeEntry()
     }
     
     override func willActivate() {
