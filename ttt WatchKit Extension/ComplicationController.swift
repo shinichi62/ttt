20a031ba698d029e252394fdc607215034fc8cee
shinichi yamaguchi <shinichi62@gmail.com>
2017-07-27 23:29:48 +0900

    delete unused func

--- ttt WatchKit Extension/ComplicationController.swift
+++ ttt WatchKit Extension/ComplicationController.swift
@@ -51,28 +51,6 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
         // This method will be called once per supported complication, and the results will be cached
         handler(nil)
-        
     }
-    
-    func getCurrentTimelineEntryForComplication(complication: CLKComplication,
-                                                withHandler handler: ((CLKComplicationTimelineEntry?) -> Void)) {
-        // Extensionデリゲートからコンプリケーションデータを取得。
-        var entry : CLKComplicationTimelineEntry?
-        let now = NSDate()
-        
-        // テンプレートおよびタイムラインエントリを生成。
-        if complication.family == .modularSmall {
-            let textTemplate = CLKComplicationTemplateModularSmallSimpleText()
-            textTemplate.textProvider = CLKSimpleTextProvider(text: "Test")
-            // エントリの生成。
-            entry = CLKComplicationTimelineEntry(date: now as Date, complicationTemplate: textTemplate)
-        }
-        else {
-            // ... 他のコンプリケーションファミリーのエントリを設定。
-        }
-        
-        // タイムラインエントリをClockKitに返却。
-        handler(entry)
-    }
-    
+
 }
