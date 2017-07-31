8e9a4ab008049ff208f6591bb771e1d91ece1e2a
shinichi yamaguchi <shinichi62@gmail.com>
2017-08-01 07:42:39 +0900

    Add LoginViewController

---
+++ ttt/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  ttt
+//
+//  Created by shinichi yamaguchi on 2017/08/01.
+//  Copyright © 2017 shinichi yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var tokenField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
